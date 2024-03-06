2441ba1ea87eff211d27ae56878da12b19961a56
axxving <ihernandezramirez800@gmail.com>
2024-03-06 15:01:01 -0600

    refactor(): Mejoras en el código
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

---
+++ .netlify/functions-internal/ssr/chunks/generic_BAlFzskt.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_DOqz32Ys.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_DIuFdHUX.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_BLI-BYFq.mjs');
+
+export { page };
