707c0a2e765e86ec9aa1d2d8dd85fbf9c8f38974
axxving <ihernandezramirez800@gmail.com>
2024-03-06 12:34:59 -0600

    feta(): Build one
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

---
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_uDGNIAw3.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./chunks/generic_BAlFzskt.mjs');
+const _page1 = () => import('./chunks/index_DBuQ6Fuz.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "a8cc784b-ec46-480b-9e94-f1220a9c5010"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
