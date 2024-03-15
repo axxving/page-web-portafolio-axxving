5026bb69d38b1d274e3bc7c810b08f813ee84fef
axxving <ihernandezramirez800@gmail.com>
2024-03-15 11:49:58 -0600

    feat(): Build 15/03/2024
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- .netlify/functions-internal/ssr/chunks/generic_CjnMWfTA.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_Ck5uezaQ.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_Bw5zADDM.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_CXZoG47M.mjs');
-
-export { page };
